43742e96bc1852d4ff6311fe562aa9fc138d50a4
Rune K. Svendsen <runesvend@gmail.com>
2024-03-21 07:59:03 +0100

    WIP: ci

--- build.nix
+++ build.nix
@@ -1,13 +1,31 @@
+# Produce a shell.nix file with all buildable Hackage packages installed into the GHC package database
+
+{ ciPickRandomCount ? null # For testing/CI: if not null then this number of random derivations will be picked out from all avaiable
+, drvListFile ? "drv-list.txt" # Write the list of successfully built Haackage package derivations to this file
+, shellFile ? "shell-tmp.nix" # The name of the Nix shell file produced
+}:
 let
   pkgs = import ./nix/pkgs.nix {};
   shellNixFile = ./shell.nix;
+  shellCmd = ''nix-shell --arg nixpkgs 'import ./nix/pkgs.nix {}' --argstr compiler ghc962 ${shellFile}'';
+  doneMessage = "Done! Run the following command to enter the shell:";
+  findNonBrokenArg = if ciPickRandomCount != null then toString ciPickRandomCount else "";
 in
 pkgs.writeScriptBin "build.sh" ''
-    #! /usr/bin/env nix-shell
-    #! nix-shell -i bash ${shellNixFile}
+  #! /usr/bin/env nix-shell
+  #! nix-shell -i bash ${shellNixFile}
 
-    set -e
+  set -euox pipefail
 
-    cabal build all && $(cabal list-bin exe:all-hackage-ghc-pkg) "$1" > shell-tmp.nix
-    nix-shell --arg nixpkgs 'import ./nix/pkgs.nix {}' --argstr compiler ghc962 shell-tmp.nix --run 'ghc-pkg list'
+  if [ ! -f "${drvListFile}" ]; then 
+    # WORKAROUND: https://github.com/NixOS/cabal2nix/issues/607
+    ./find-non-broken.sh ${findNonBrokenArg} | grep --line-buffered -v -e '-if-0.1.0.0.drv' > ${drvListFile}
+  else
+    echo "INFO: The file ${drvListFile} already exists. Skipping 'find non-broken'-step. Remove the file to run this step." >&2
+  fi
+  cabal build all >&2
+  $(cabal list-bin exe:all-hackage-ghc-pkg) ${drvListFile} > ${shellFile}
+  # WORKAROUND: see README.md
+  sed -e 's/hercules-ci-cnix-expr, //' -e 's/hercules-ci-cnix-expr//' -i shell-tmp.nix
+  ${shellCmd} --run "echo -e \"${doneMessage}\" >&2; echo -n \"${shellCmd}\""
 ''
---
+++ test/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DRV_LIST_FILE="drv-list-test.txt"
+NUM_DRVS=10
+
+mv "$DRV_LIST_FILE" "$DRV_LIST_FILE.bak"
+
+SHELL_CMD=$("$(nix-build --arg ciPickRandomCount $NUM_DRVS --arg drvListFile \"$DRV_LIST_FILE\" build.nix)/bin/build.sh")
+
+if ! grep --silent -F "/nix/store/" "$DRV_LIST_FILE" ; then
+  echo "Derivation list file does not contain any derivations. Perhaps increase NUM_DRVS?"
+  exit 1
+fi
+
+PACKAGE_NAMES=$(sed -rn 's|/nix/store/[0-9a-z]+-([a-z0-9\.\-]+)\.drv|\1|p' "$DRV_LIST_FILE")
+SHELL_PACKAGES=$(eval "$SHELL_CMD --run 'ghc-pkg list --simple-output'")
+
+for pkg in $PACKAGE_NAMES; do
+  if ! grep --silent -F "$pkg" <(echo "$SHELL_PACKAGES") ; then
+    echo "FAIL: Missing package $pkg in shell package list: $SHELL_PACKAGES"
+    exit 1
+  fi
+done
+
