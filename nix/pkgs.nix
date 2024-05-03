50932f0ba9b0081760dc365614bfd9ff51076537
Rune K. Svendsen <runesvend@gmail.com>
2024-05-03 12:59:48 +0200

    Update fork to c3a1749f84f968949551f825777b56171fe7a95f

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,8 +1,8 @@
 let
   unstable =
     import (builtins.fetchTarball {
-      url = "https://github.com/runeksvendsen/nixpkgs/archive/b574a891f9d1172e455234b282774d588cd5c150.tar.gz";
-      sha256 = "1j81mwzf7w7wyw9pkjbhhbr2hpvffnw062jlvgric2sk976dxfzr";
+      url = "https://github.com/runeksvendsen/nixpkgs/archive/c3a1749f84f968949551f825777b56171fe7a95f.tar.gz";
+      sha256 = "1lr64nkkw08402x49q5l08sl85irg84ahlv39mvx7yqn9is00vj4";
     });
 in
   unstable
