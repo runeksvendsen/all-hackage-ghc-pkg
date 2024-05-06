a31b536f15f4856b85de3868123c14f1a7f73f09
Rune K. Svendsen <runesvend@gmail.com>
2024-05-06 08:12:33 +0200

    Update nixpkgs fork to d1b139547d1cfacef42bfd4761461691811547fe

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,8 +1,8 @@
 let
   unstable =
     import (builtins.fetchTarball {
-      url = "https://github.com/runeksvendsen/nixpkgs/archive/c3a1749f84f968949551f825777b56171fe7a95f.tar.gz";
-      sha256 = "1lr64nkkw08402x49q5l08sl85irg84ahlv39mvx7yqn9is00vj4";
+      url = "https://github.com/runeksvendsen/nixpkgs/archive/d1b139547d1cfacef42bfd4761461691811547fe.tar.gz";
+      sha256 = "092zmbgsy3vgn4ly7g41f43npcrji54q00d622pivzhvgsy27gpz";
     });
 in
   unstable
